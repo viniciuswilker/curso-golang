de453791bb4e8a5152093ccbe27514e55a9934b4
vinicius wilker <vinicius.zoom360@gmail.com>
2026-02-10 19:33:27 -0300

    ajuste pastas e inicio teste

---
+++ 2 - testes/1 - introducao/enderecos/enderecos.go
@@ -0,0 +1,26 @@
+package enderecos
+
+import "strings"
+
+func TipoDeEndereco(endereco string) string {
+
+	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
+
+	enderecoEmLetraMinuscula := strings.ToLower(endereco)
+
+	primeiraPalavraDoEndereco := strings.Split(enderecoEmLetraMinuscula, " ")[0]
+
+	enderecoTemUmTipoValido := false
+
+	for _, tipo := range tiposValidos {
+		if tipo == primeiraPalavraDoEndereco {
+			enderecoTemUmTipoValido = true
+		}
+	}
+
+	if enderecoTemUmTipoValido {
+		return primeiraPalavraDoEndereco
+	}
+
+	return "Tipo Inválido"
+}
