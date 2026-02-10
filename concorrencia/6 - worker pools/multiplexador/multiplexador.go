3fb0708b8ab11f55e6e780a692145c92cc7427b3
vinicius wilker <vinicius.zoom360@gmail.com>
2026-02-10 19:20:25 -0300

    genetaros/ worker pools

---
+++ concorrencia/6 - worker pools/multiplexador/multiplexador.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
+func main() {
+
+	canal := multiplexar(escrever("Olá mundo"), escrever("Programando em GO"))
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-canal)
+
+	}
+
+}
+
+func multiplexar(canalDeEntrada1, canalDeEntrada2 <-chan string) <-chan string {
+
+	canalDeSaida := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case mensagem := <-canalDeEntrada1:
+				canalDeSaida <- mensagem
+			case mensagem := <-canalDeEntrada2:
+				canalDeSaida <- mensagem
+			}
+		}
+	}()
+
+	return canalDeSaida
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintf("Valor recebido : %s ", texto)
+			time.Sleep(time.Millisecond * time.Duration(rand.IntN(2000)))
+		}
+	}()
+	return canal
+
+}
