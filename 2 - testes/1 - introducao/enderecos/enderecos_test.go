700f256d8f4c12f962651baa8b29127542e7fe7c
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-10 20:46:58 -0300

    testes intro

--- 2 - testes/1 - introducao/enderecos/enderecos_test.go
+++ 2 - testes/1 - introducao/enderecos/enderecos_test.go
@@ -2,17 +2,32 @@ package enderecos
 
 import "testing"
 
+type cenarioDeTeste struct {
+	enderecoInserido string
+	enderecoEsperado string
+}
+
 func TestTipoDeEndereco(t *testing.T) {
 	// Test + NomeDaFuncao  recebe t *testing.T
 
-	enderecoParaTeste := "Avenida Paulista"
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia ABC", "Rodovia"},
+		{"Praça das rosas", "Tipo Inválido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"Praça das rosas", "Tipo Inválido"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{" ", "Tipo Inválido"},
+	}
 
-	tipoDeEnderecoEsperado := "Avenida"
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.enderecoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s", tipoDeEnderecoRecebido, cenario.enderecoEsperado)
 
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
-
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Error("O tipo recebido é diferente do esperado")
+		}
 	}
 
 }
--- 2 - testes/1 - introducao/introducao.go
+++ 2 - testes/1 - introducao/introducao.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"introducao-testes/enderecos"
+)
 
-func main(){
-	
+func main() {
+
+	tipoEndereco := enderecos.TipoDeEndereco("Avenida Paulista")
+
+	fmt.Println(tipoEndereco)
+
 }
