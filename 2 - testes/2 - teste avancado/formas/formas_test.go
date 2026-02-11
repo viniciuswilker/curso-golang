4da9f10977068a293c486b404a25b1c6e8dafa78
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-11 08:21:44 -0300

    final modulo teste

---
+++ 2 - testes/2 - teste avancado/formas/formas_test.go
@@ -0,0 +1,34 @@
+package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	t.Run("Retângulo", func(t *testing.T) {
+
+		ret := Retangulo{10, 12}
+		areaEsperada := float64(120)
+		areaRecebida := ret.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Errorf("A area recebida %f é diferente da esperada %f ", areaRecebida, areaEsperada)
+		}
+
+	})
+
+	t.Run("Ciculo", func(t *testing.T) {
+
+		cir := Circulo{10}
+
+		areaEsperada := float64(math.Pi * 100)
+		areaRecebida := cir.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Errorf("A area recebida %f é diferente da esperada %f ", areaRecebida, areaEsperada)
+		}
+
+	})
+
+}
