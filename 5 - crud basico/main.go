5cc212c172fd696dcc77cdee88e189e0e3cb4ce2
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-12 21:45:40 -0300

    func de inserir usuario no banco

---
+++ 5 - crud basico/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"crud-basico/servidor"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+
+	fmt.Println("Escutando a porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+
+}
---
+++ 5 - crud basico/servidor/servidor.go
@@ -0,0 +1,61 @@
+package servidor
+
+import (
+	"crud-basico/banco"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type usuario struct {
+	ID    uint32 `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+}
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Falha ao ler o corpo ta requisição"))
+		return
+	}
+
+	var usuario usuario
+
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter usuario para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco"))
+		return
+	}
+	defer db.Close()
+
+	statament, erro := db.Prepare("insert into usuarios (nome, email) values (?,?)")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statament.Close()
+
+	insercao, erro := statament.Exec(usuario.Nome, usuario.Email)
+	if erro != nil {
+		w.Write([]byte("Erro ao executar o statement"))
+		return
+	}
+
+	idInserido, erro := insercao.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("Erro ao obter o ID inserido"))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Usuario inserido com sucesso! Id: %d", idInserido)))
+
+}
